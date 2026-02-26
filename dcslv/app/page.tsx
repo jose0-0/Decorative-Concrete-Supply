b888b46279f717d413b81c120bb5ef551e2367fa
Jose Arguetaq <jose.argueta@ionnovate.com>
2026-02-26 15:32:01 -0800

    included additonal file changes from previous commit

--- dcslv/app/page.tsx
+++ dcslv/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
-  );
+  return <div className="h-full"></div>;
 }
